e2b3ae9cb96440a3229febedc2b272249303a0fc
Luis Ulloa <timken@gmail.com>
2018-07-31 12:33:24 +0100

    Better setup

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
-import os
-import sys
-import subprocess
 from package_settings import NAME, VERSION, PACKAGES, DESCRIPTION
 from setuptools import setup
+from pathlib import Path
+import json
+import urllib.request
+from functools import lru_cache
 
-# # TODO is there a better way ? dependencies seem to always require the version
-# # Calling only at the egg_info step gives us the wanted depth first behavior
-# if 'egg_info' in sys.argv and os.getenv('CAPE_DEPENDENCIES', 'False').lower() == 'true':
-#     subprocess.check_call(['pip3', 'install','--no-warn-conflicts','--upgrade', '-r', 'requirements.txt'])
+
+@lru_cache
+def _get_github_sha(github_install_url: str):
+    """From the github_install_url get the hash of the latest commit"""
+    repository = Path(github_install_url).stem.split('#egg', 1)[0]
+    organisation = Path(github_install_url).parent.stem
+    with urllib.request.urlopen(f'https://api.github.com/repos/{organisation}/{repository}/commits/master') as response:
+        return json.loads(response.read())['sha']
+
 
 setup(
     name=NAME,
@@ -23,12 +29,15 @@ setup(
         'scout==3.0.2',
         'dataclasses==0.6',
         'cytoolz==0.9.0.1',
-        'cape_splitter',
-        'cape_api_helpers',
+        'cape_splitter==' + _get_github_sha('git+https://github.com/bloomsburyai/cape-splitter#egg=cape_splitter'),
+        'cape_api_helpers==' + _get_github_sha(
+            'git+https://github.com/bloomsburyai/cape-api-helpers#egg=cape_api_helpers'),
     ],
     dependency_links=[
-        'git+https://github.com/bloomsburyai/cape-splitter#egg=cape_splitter',
-        'git+https://github.com/bloomsburyai/cape-api-helpers#egg=cape_api_helpers'
+        'git+https://github.com/bloomsburyai/cape-splitter#egg=cape_splitter-' + _get_github_sha(
+            'git+https://github.com/bloomsburyai/cape-splitter#egg=cape_splitter'),
+        'git+https://github.com/bloomsburyai/cape-api-helpers#egg=cape_api_helpers-' + _get_github_sha(
+            'git+https://github.com/bloomsburyai/cape-api-helpers#egg=cape_api_helpers')
     ],
     package_data={
         '': ['*.*'],
